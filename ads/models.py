0a8f1e97ffe518a09ecd5e227842d9f8d45e100c
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-02-10 20:34:08 +0300

    db migrated

---
+++ ads/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+class Ads(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.CharField(max_length=1000)
+    address = models.CharField(max_length=120)
+    is_published = models.BooleanField(default=False)
+
+
+class Cat(models.Model):
+    name = models.CharField(max_length=120)
