da851f5a42184adb251cccf348e56627a84fe740
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-02-12 14:30:52 +0300

    added the root view

--- skyvito/urls.py
+++ skyvito/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from ads import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("ads/", include("ads.urls")),
+    path("", views.index, name="index")
 ]
