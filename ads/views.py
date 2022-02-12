afcefa00043badca0bc6533348ba1f5077798a50
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-02-13 02:00:29 +0300

    added a pretty_json_response func

--- ads/views.py
+++ ads/views.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Union
 from pydantic import BaseModel, Field
 import json
 
@@ -17,6 +17,18 @@ ADO = Ad.objects
 CATO = Cat.objects
 
 
+def pretty_json_response(json_data: Union[dict, list[dict]]) -> JsonResponse:
+    """
+    a shortcut to JsonResponse with json dumps parameters
+    """
+
+    return JsonResponse(
+        json_data,
+        safe=False,
+        json_dumps_params={"ensure_ascii": False, "indent": 2}  # чтобы вывести кириллицу в браузере + отступы
+    )
+
+
 class AdModel(BaseModel):
     pk: Optional[int] = Field(alias="id")
     name: str
@@ -30,11 +42,12 @@ class AdModel(BaseModel):
         orm_mode = True
 
 
-class AdsModel(BaseModel):
-    item: list[AdModel]
-
-    class Config:
-        orm_mode = True
+# an attempt to serialize pydantic models
+# class AdsModel(BaseModel):
+#     item: list[AdModel]
+#
+#     class Config:
+#         orm_mode = True
 
 
 class CatModel(BaseModel):
@@ -53,47 +66,41 @@ def index(request):
 class AdView(View):
     @staticmethod
     def get(request):
-        return JsonResponse(
-            [AdModel.from_orm(ad).dict() for ad in ADO.all()],
-            safe=False,
-            json_dumps_params={"ensure_ascii": False, "indent": 2}  # чтобы вывести кириллицу в браузере + отступы
-        )
+        return pretty_json_response([AdModel.from_orm(ad).dict() for ad in ADO.all()])
 
     @staticmethod
     def post(request):
         ad = ADO.create(**AdModel.parse_raw(request.body).dict())
-        return JsonResponse(AdModel.from_orm(ad).dict())
+        return pretty_json_response(AdModel.from_orm(ad).dict())
 
 
 class AdDetailView(DetailView):
     model = Ad
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs) -> JsonResponse:
         ad = self.get_object()
-        return JsonResponse(AdModel.from_orm(ad).dict(), json_dumps_params={"ensure_ascii": False, "indent": 2})
+        return pretty_json_response(AdModel.from_orm(ad).dict())
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class AdHTTPJsonView(View):
     @staticmethod
-    def get(request):
-        if name := request.GET.get("name", None):
-            res_obj = ADO.filter(name=name)
-        else:
-            res_obj = ADO.all()
-
+    def get(request) -> HttpResponse:
+        res_obj = ADO.filter(name=name) if (name := request.GET.get("name", None)) else ADO.all()
         s_dicts = [AdModel.from_orm(ad).dict(include={"pk", "name", "price"}) for ad in res_obj]
+
+        # an attempt to serialize pydantic models
         # s_dicts = [{"item": AdModel.from_orm(ad)} for ad in res_obj]
         # s_dicts = AdsModel.from_orm(s_dicts)
-        s = json.dumps(s_dicts, ensure_ascii=False, indent=2)
 
+        s = json.dumps(s_dicts, ensure_ascii=False, indent=2)
         return HttpResponse(s, content_type='text/plain; charset=utf-8')
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class AdHTTPView(View):
     @staticmethod
-    def get(request):
+    def get(request) -> HttpResponse:
         res_obj = ADO.filter(name=name) if (name := request.GET.get("name", None)) else ADO.all()
         return render(request, 'ads_list.html', {"ads": res_obj})
 
@@ -101,7 +108,7 @@ class AdHTTPView(View):
 class AdHTTPJsonDetailView(DetailView):
     model = Ad
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs) -> HttpResponse:
         s = AdModel.from_orm(self.get_object()).json(ensure_ascii=False, indent='\t')
         return HttpResponse(s, content_type='text/plain; charset=utf-8')
 
@@ -109,21 +116,18 @@ class AdHTTPJsonDetailView(DetailView):
 @method_decorator(csrf_exempt, name='dispatch')
 class CatView(View):
     @staticmethod
-    def get(request):
-        return JsonResponse(
-            [CatModel.from_orm(cat).dict() for cat in CATO.all()],
-            safe=False
-        )
+    def get(request) -> JsonResponse:
+        return pretty_json_response([CatModel.from_orm(cat).dict() for cat in CATO.all()])
 
     @staticmethod
-    def post(request):
+    def post(request) -> JsonResponse:
         cat = CATO.create(**CatModel.parse_raw(request.body).dict())
-        return JsonResponse(CatModel.from_orm(cat).dict())
+        return pretty_json_response(CatModel.from_orm(cat).dict())
 
 
 class CatDetailView(DetailView):
     model = Cat
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs) -> JsonResponse:
         cat = self.get_object()
-        return JsonResponse(CatModel.from_orm(cat).dict())
+        return pretty_json_response(CatModel.from_orm(cat).dict())
