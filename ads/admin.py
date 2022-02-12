82faa31129079bd539ef246ef62f2da3da254e25
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-02-12 14:26:34 +0300

    added Meta for Ad, verbose_names, AdAdmin

--- ads/admin.py
+++ ads/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Ad, Cat
 
-admin.site.register(Ad)
+
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'price', 'description')
+    list_display_links = ('name', 'author')
+    search_fields = ('name', 'author', 'description')
+
+
+admin.site.register(Ad, AdAdmin)
 admin.site.register(Cat)
--- ads/models.py
+++ ads/models.py
@@ -2,16 +2,21 @@ from django.db import models
 
 
 class Ad(models.Model):
-    name = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    price = models.IntegerField()
-    description = models.CharField(max_length=1000)
-    address = models.CharField(max_length=120)
-    is_published = models.BooleanField(default=False)
+    name = models.CharField(max_length=100, verbose_name="Объявление")
+    author = models.CharField(max_length=100, verbose_name="Автор")
+    price = models.IntegerField(verbose_name="Цена")
+    description = models.CharField(max_length=1000, verbose_name="Описание")
+    address = models.CharField(max_length=120, verbose_name="Адрес")
+    is_published = models.BooleanField(default=False, verbose_name="Опубликован или нет")
+
+    class Meta:
+        verbose_name_plural = "Объявления"
+        verbose_name = "Объявление"
+        ordering = ["pk"]
 
 
 class Cat(models.Model):
-    name = models.CharField(max_length=120)
+    name = models.CharField(max_length=120, verbose_name="Категория")
 
     def __str__(self):
         return self.name
