6989881e484a204c2c51732cddb92dc895b56c2d
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-02-13 19:28:56 +0300

    added a foreign key  category_id

--- ads/models.py
+++ ads/models.py
@@ -5,9 +5,10 @@ class Ad(models.Model):
     name = models.CharField(max_length=100, verbose_name="Объявление")
     author = models.CharField(max_length=100, verbose_name="Автор")
     price = models.IntegerField(verbose_name="Цена")
-    description = models.CharField(max_length=1000, verbose_name="Описание")
+    description = models.CharField(max_length=1000, verbose_name="Описание", null=True, blank=True)
     address = models.CharField(max_length=120, verbose_name="Адрес")
     is_published = models.BooleanField(default=False, verbose_name="Опубликован или нет")
+    category_id = models.ForeignKey('Cat', on_delete=models.PROTECT, verbose_name="Категория")
 
     class Meta:
         verbose_name_plural = "Объявления"
